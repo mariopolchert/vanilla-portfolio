52bacfec368e794cb7467ce4c6827fd5e2db7a27
Mario Polchert <mario.polchert@gmail.com>
2023-10-08 14:45:42 +0200

    started fresh, made mobile, menu and sections working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vanilla-portfolio
+
+Portfolio website, made with basic html, css and javascript. Copy of my github page for a start.
--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-window.onscroll = () => {
-    scrollingMenu();
-};
 const nav = document.querySelector("nav");
-const logo = document.querySelector("#logo");
+const mobileNav = document.querySelector("#mobileNav");
+const mobileLinks = document.querySelectorAll(".mobileLinks");
+const divHam = document.querySelector("#divHam");
+const mobileLogoLink = document.querySelector("#mobileLogoLink");
 
-function scrollingMenu() {
-    if (
-        document.body.scrollTop > 80 ||
-        document.documentElement.scrollTop > 80
-    ) {
-        // reduced menu
-        nav.classList.remove("normalMenu");
-        nav.classList.add("reducedMenu");
-        // logo.src = "http://placehold.co/30x30";
-    } else {
-        // normal menu
-        nav.classList.add("normalMenu");
-        nav.classList.remove("reducedMenu");
-        // logo.src = "http://placehold.co/100x100";
-    }
-}
+// window.onscroll = () => {
+//     scrollingMenu();
+// };
+
+// function scrollingMenu() {
+//     if (
+//         document.body.scrollTop > 80 ||
+//         document.documentElement.scrollTop > 80
+//     ) {
+//         // reduced menu
+//         nav.classList.remove("normalMenu");
+//         nav.classList.add("reducedMenu");
+//     } else {
+//         // normal menu
+//         nav.classList.add("normalMenu");
+//         nav.classList.remove("reducedMenu");
+//     }
+// }
+
+const handleClick = () => {
+    divHam.classList.toggle("xsign");
+    mobileNav.classList.toggle("active");
+};
+
+mobileLogoLink.addEventListener("click", () => {
+    divHam.classList.remove("xsign");
+    mobileNav.classList.remove("active");
+});
+
+divHam.addEventListener("click", () => {
+    handleClick();
+});
+
+mobileLinks.forEach((n) => {
+    n.addEventListener("click", () => {
+        handleClick();
+    });
+});
