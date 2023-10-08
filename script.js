611c0b4b0159bc3bcfe765a886060792137c24da
Mario Polchert <mario.polchert@gmail.com>
2023-10-08 20:11:48 +0200

    basic navigation working and responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("nav");
+const desktopMenu = document.querySelector("#desktopMenu");
 const mobileNav = document.querySelector("#mobileNav");
 const mobileLinks = document.querySelectorAll(".mobileLinks");
 const divHam = document.querySelector("#divHam");
@@ -9,19 +9,19 @@ const mobileLogoLink = document.querySelector("#mobileLogoLink");
 // };
 
 // function scrollingMenu() {
-//     if (
-//         document.body.scrollTop > 80 ||
-//         document.documentElement.scrollTop > 80
-//     ) {
-//         // reduced menu
-//         nav.classList.remove("normalMenu");
-//         nav.classList.add("reducedMenu");
-//     } else {
-//         // normal menu
-//         nav.classList.add("normalMenu");
-//         nav.classList.remove("reducedMenu");
-//     }
+//     document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
+//         ? desktopMenu.classList.add("reduced")
+//         : desktopMenu.classList.remove("reduced");
 // }
+
+// JavaScript logic to add the 'reduced' class on scroll
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+        desktopMenu.classList.add("reduced");
+    } else {
+        desktopMenu.classList.remove("reduced"); // Remove the class
+    }
+});
 
 const handleClick = () => {
     divHam.classList.toggle("xsign");
